4ef26f15bc1c9a70927e054a93049615ee258de5
Efrem Thinkpad <BarefootEfrem@gmail.com>
2022-04-20 09:47:59 -0700

    format with prettier

--- src/leaflet-areaselect.d.ts
+++ src/leaflet-areaselect.d.ts
@@ -1,36 +1,36 @@
-import * as L from "leaflet";
+import * as L from "leaflet"
 
 declare module "leaflet" {
-  function areaSelect(box: AreaSelectOptions): AreaSelect;
+  function areaSelect(box: AreaSelectOptions): AreaSelect
 
   type AreaSelectOptions = {
-    width: number;
-    height: number;
-    minWidth: number;
-    minHeight: number;
-    minHorizontalSpacing: number;
-    minVerticalSpacing: number;
-    keepAspectRatio: boolean;
-  };
+    width: number
+    height: number
+    minWidth: number
+    minHeight: number
+    minHorizontalSpacing: number
+    minVerticalSpacing: number
+    keepAspectRatio: boolean
+  }
 
   type Dimensions = {
-    width: number;
-    height: number;
-  };
+    width: number
+    height: number
+  }
 
   type Corners = {
-    sw: L.LatLng;
-    nw: L.LatLng;
-    ne: L.LatLng;
-    se: L.LatLng;
-  };
+    sw: L.LatLng
+    nw: L.LatLng
+    ne: L.LatLng
+    se: L.LatLng
+  }
 
   class AreaSelect extends L.Evented {
-    constructor(options?: Partial<AreaSelectOptions>);
-    addTo(map: L.Map): L.Map;
-    revove(): void;
-    getBounds(): L.LatLngBounds;
-    setDimensions(dimensions: Dimensions): void;
-    getBBoxCoordinates(): Corners;
+    constructor(options?: Partial<AreaSelectOptions>)
+    addTo(map: L.Map): L.Map
+    revove(): void
+    getBounds(): L.LatLngBounds
+    setDimensions(dimensions: Dimensions): void
+    getBBoxCoordinates(): Corners
   }
 }
